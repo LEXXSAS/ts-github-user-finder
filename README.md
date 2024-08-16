b24674fbfd9405ac477244187e9bc3f310c28cc0
Alexey Sedov <ASedov15@gmail.com>
2024-08-16 12:31:54 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# TypeScript курс. Проект GitHub user finder
+
+> [!NOTE]
+> To start the project you need to run "node init -y" and "node install"
+After that "npm start"
---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGitHubUser } from "types";
+
+export const defaultUser: LocalGitHubUser = {
+    "login": "LEXXSAS",
+    "avatar": "https://avatars.githubusercontent.com/u/93075959?v=4",
+    "name": "Alexey",
+    "company": null,
+    "blog": "https://portfolio.1lexxs.com",
+    "location": "Russia",
+    "bio": null,
+    "twitter": null,
+    "repos": 48,
+    "followers": 0,
+    "following": 0,
+    "created": "2021-10-24T09:36:54Z",
+}
